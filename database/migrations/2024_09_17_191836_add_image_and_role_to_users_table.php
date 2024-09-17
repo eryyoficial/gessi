82a07444436dacd2eefad96d1a0afd49813f6ba3
Hermínio Da Silva André Francisco <154982502+eryyoficial@users.noreply.github.com>
2024-09-17 20:24:16 +0100

    update

---
+++ database/migrations/2024_09_17_191836_add_image_and_role_to_users_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('image')->nullable()->after('email'); // Adiciona o campo para o caminho da imagem
+            $table->enum('role', ['admin', 'doador', 'visit'])->default('visit')->after('password'); // Adiciona o campo de role
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['image', 'role']);
+        });
+    }
+};
